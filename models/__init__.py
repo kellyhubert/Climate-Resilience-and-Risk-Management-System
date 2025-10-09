bf9bd165b6c0adb9abcd58a5c3a5b40f8cdbbda2
kellyhubert <irakellyhub@gmail.com>
2025-10-09 22:45:05 +0200

    updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 """RWanda Climate Risk Models Package."""
-from .base_model import BaseModel
+from .base_model import BaseRiskModel
 from .landslide_model import RwandaLandslideModel
 from .flood_model import RwandaFloodModel
 from .drought_model import RwandaDroughtModel
-from .ensemble_model import RwandaEnsembleModel
+from .ensemble_model import RwandaMultiHazardModel
 
 ___all__ = [
     
